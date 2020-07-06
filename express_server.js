50ac76585d2dea5677b7c1a706647212c751ff1c
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-06 20:23:43 +0000

    adding partial templates for scripts and headers

--- express_server.js
+++ express_server.js
@@ -18,6 +18,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  console.log('shortURL', shortURL);
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });

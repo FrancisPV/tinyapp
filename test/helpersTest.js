e23ce26955cab9af37bab390fd86d56f8f1ff2d4
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-09 13:42:54 -0400

    created a test to verifry come component

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helper.js');
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', () => {
+  it('should return a user with valid email', () => {
+    const actual = findUserByEmail("user@example.com", users);
+    const expected = "user@example.com";
+    assert(actual.email === expected);
+  });
+});
+
+describe('findUserByEmail', () => {
+  it('should return undefined if the email doesn\'t exist', () => {
+    const actual = findUserByEmail("Whatever", users);
+    const expected = undefined;
+    assert(actual === expected);
+  });
+});

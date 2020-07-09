75de557f8b0ba9715718e9cd904cf81385eab40b
Francis Pellerin-Venne <francispellerinvenne@hotmail.fr>
2020-07-09 13:42:18 -0400

    created a helper file to put all my helper function

---
+++ helper.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const urlsForUser = (id, urlDatabase) => {
+  const urlObject = {};
+  for (const key in urlDatabase) {
+    
+    if (urlDatabase[key] && urlDatabase[key].userID === id) {
+      urlObject[key] = urlDatabase[key];
+    }
+  }
+  return urlObject;
+};
+
+//authenticate the user
+const authenticateUser = (email, password, users) => {
+  const user = findUserByEmail(email, users);
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    return user.id;
+  }
+};
+
+
+const generateRandomString = () => {
+  return Math.random().toString(36).substr(2, 6);
+};
+
+const addNewUser = (email, password, username, users) => {
+  const userId = generateRandomString();
+  const user = { id: userId, email, username, password: bcrypt.hashSync(password, saltRounds) };
+
+  users[userId] = user;
+  return userId;
+};
+
+const findUserByEmail = (email, users) => {
+  for (let userId in users) {  // need to create the user object
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  return;
+};
+
+
+module.exports = { urlsForUser, generateRandomString, addNewUser, findUserByEmail, authenticateUser };
